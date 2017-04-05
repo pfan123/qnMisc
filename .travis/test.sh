6f0d5177aaa8240168fa1e11fb551a800fd335e7
pfan <768065158@qq.com>
2017-04-05 10:41:11 +0800

    modify test.sh

--- .travis/test.sh
+++ .travis/test.sh
@@ -6,6 +6,7 @@ git config user.name "pfan123"
 git config user.email "768065158@qq.com"
 git add .
 git commit -m "Deployed from Travis CI"
-git push --force --quiet "git@github.com:pfan123/qnMisc.git" master:test
+git push --force --quiet "https://${DEPLOY_TOKEN}@github.com:pfan123/qnMisc.git" master:test
 
 echo "发布完成"
+DEPLOY_TOKEN
