72813525263d20438348d3a672a57fb1361b622d
pfan <768065158@qq.com>
2017-04-05 09:46:53 +0800

    modify test.sh

--- .travis/test.sh
+++ .travis/test.sh
@@ -1,1 +1,15 @@
-echo "测试结束"
+#!/bin/bash
+if [ "$1" = "false" ]
+	
+# not a pull request, deploy to github pages
+then ( cd dist
+  git init
+  git config user.name "pfan123"
+  git config user.email "768065158@qq.com"
+  git add .
+  git commit -m "Deployed from Travis CI"
+  git push --force --quiet "git@github.com:pfan123/travis-test.git" master:master
+
+  echo "发布完成"
+)
+fi
